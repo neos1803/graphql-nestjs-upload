ceb6a4bb5a5d3b8ad581b7e642bbd47e72700e20
Ardi <oliviergiroud1998@gmail.com>
2021-08-17 17:30:46 +0700

    Comment incomplete interceptor

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -1,17 +1,25 @@
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { FileUpload, GraphQLUpload } from "graphql-upload";
 import { createWriteStream } from "fs";
+// import { UseInterceptors } from "@nestjs/common";
+// import { SingleFileInterceptor } from "src/interceptors/request.inteceptors";
 
 @Resolver()
 export class AppResolver {
 
   @Mutation(returns => Boolean)
+  // @UseInterceptors(SingleFileInterceptor('file'))
   async uploadFile(
     @Args('file', { type: () => GraphQLUpload }) file: FileUpload
   ) {
-    console.log(file)
-    const file_name = await this.uploadFileHelper(file);
-    return true;
+    try {
+      // console.log(file)
+      // const file_name = await this.uploadFileHelper(file);
+      console.log(1)
+      return true;
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 
   private uploadFileHelper({ createReadStream, encoding, filename, mimetype }: FileUpload): Promise<string> {
