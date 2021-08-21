ec0a159cedbd4e8e7c1dcac39a0daed723731765
Ardi <oliviergiroud1998@gmail.com>
2021-08-21 11:09:58 +0700

    Add multiple field upload

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -12,23 +12,23 @@ export class AppResolver {
     @Args('file', { type: () => GraphQLUpload }) file: FileUpload
   ) {
     try {
-      if (file.filename.split(".").pop() != 'png') throw new BadRequestException("File type musst be png");
+      if (file.filename.split(".").pop() != 'png') throw new BadRequestException("File type must be png");
       await fileSizeValidation(file.createReadStream(), 1000).catch((e) => {
         throw new BadRequestException(e)
       });
 
-      const file_name = await this.uploadFileHelper(file);
+      const file_name = await this.uploadFileHelper(file, 'file');
       return true;
     } catch (error) {
       throw new InternalServerErrorException(error.message);
     }
   }
 
-  private uploadFileHelper({ createReadStream, encoding, filename, mimetype }: FileUpload): Promise<string> {
+  private uploadFileHelper({ createReadStream, encoding, filename, mimetype }: FileUpload, field: string): Promise<string> {
     console.log(mimetype);
     const fName = `${Date.now()}-file.${mimetype.split('/')[1]}`
     const upload: Promise<string> = new Promise((resolve, reject) => createReadStream()
-      .pipe(createWriteStream('public/file/' + fName))
+      .pipe(createWriteStream(`public/${field}/` + fName))
       .on('finish', async () => {
         resolve(fName)        
       })
@@ -38,6 +38,30 @@ export class AppResolver {
     )
 
     return upload
+  }
+
+  @Mutation(returns => Boolean)
+  async multipleFieldUpload(
+    @Args('photo', { type: () => GraphQLUpload, nullable: true }) photo: FileUpload,
+    @Args('document', { type: () => GraphQLUpload, nullable: true }) document: FileUpload,
+  ) {
+    try {
+      if (photo.filename.split(".").pop() != 'png') throw new BadRequestException("Photo type must be png");
+      await fileSizeValidation(photo.createReadStream(), 1000).catch((e) => {
+        throw new BadRequestException(e)
+      });
+
+      if (document.filename.split(".").pop() != 'pdf') throw new BadRequestException("Document type musst be pdf");
+      await fileSizeValidation(document.createReadStream(), 1000).catch((e) => {
+        throw new BadRequestException(e)
+      });
+
+      await this.uploadFileHelper(photo, 'photo');
+      await this.uploadFileHelper(document, 'document');
+      return true;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   @Query(() => String)
